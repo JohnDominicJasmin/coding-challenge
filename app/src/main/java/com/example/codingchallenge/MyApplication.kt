6796fed5d536d84c570bc81ff42fa82fa0427be9
JohnDominicJasmin <johndominicjasmin@gmail.com>
2025-01-14 00:01:44 +0800

    Add app application and initialize hilt android app

--- app/src/main/java/com/example/codingchallenge/MyApplication.kt
+++ app/src/main/java/com/example/codingchallenge/MyApplication.kt
@@ -2,5 +2,5 @@ package com.example.codingchallenge
 
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
-@HiltAndroid
-class MyApp : Application()
+@HiltAndroidApp
+class MyApplication : Application()
