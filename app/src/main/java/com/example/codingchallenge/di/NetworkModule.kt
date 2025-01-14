de1c848cfe3c6ab7c08b151f1da5b64f72cc22be
JohnDominicJasmin <johndominicjasmin@gmail.com>
2025-01-14 21:45:36 +0800

    Add DI module

---
+++ app/src/main/java/com/example/codingchallenge/di/NetworkModule.kt
@@ -0,0 +1,81 @@
+package com.example.codingchallenge.di
+
+import android.content.Context
+import com.example.codingchallenge.di.Constants.HEADER_CACHE_CONTROL
+import com.example.codingchallenge.di.Constants.HEADER_PRAGMA
+import com.example.codingchallenge.feature_users.data.RandomUserApiService
+import com.example.codingchallenge.feature_users.data.data_source.local.NetworkConnectivity
+import com.example.codingchallenge.feature_users.data.repository.UserRepositoryImpl
+import com.example.codingchallenge.feature_users.domain.repository.UserRepository
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import okhttp3.Cache
+import okhttp3.CacheControl
+import okhttp3.Interceptor
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.io.File
+import java.util.concurrent.TimeUnit
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object NetworkModule {
+    @Provides
+    @Singleton
+    fun provideRetrofit(okHttpClient: OkHttpClient): RandomUserApiService {
+        return lazy {
+            Retrofit.Builder()
+                .baseUrl(Constants.BASE_URL)
+                .client(okHttpClient)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build()
+                .create(RandomUserApiService::class.java)
+        }.value
+    }
+
+    @Provides
+    @Singleton
+    fun provideUserRepository(api: RandomUserApiService): UserRepository {
+        return UserRepositoryImpl(
+            api = api
+        )
+    }
+
+    @Provides
+    @Singleton
+    fun providesOkhttpClient(@ApplicationContext context: Context): OkHttpClient {
+        val interceptor = Interceptor { chain ->
+            var request = chain.request()
+            if (!NetworkConnectivity(context).hasInternet()) {
+                val cacheControl = CacheControl.Builder()
+                    .maxStale(1, TimeUnit.DAYS)
+                    .build()
+
+                request = request.newBuilder()
+                    .removeHeader(HEADER_PRAGMA)
+                    .removeHeader(HEADER_CACHE_CONTROL)
+                    .cacheControl(cacheControl)
+                    .build()
+            }
+            chain.proceed(request)
+        }
+        val httpCacheDirectory = File(context.cacheDir, "offlineCache")
+        val cacheSize = 50 * 1024 * 1024
+        val cache = Cache(httpCacheDirectory, cacheSize.toLong())
+
+        return lazy {
+            OkHttpClient.Builder()
+                .cache(cache)
+                .connectTimeout(8, TimeUnit.SECONDS)
+                .writeTimeout(8, TimeUnit.SECONDS)
+                .readTimeout(8, TimeUnit.SECONDS)
+                .addInterceptor(interceptor)
+                .build()
+        }.value
+    }
+}
