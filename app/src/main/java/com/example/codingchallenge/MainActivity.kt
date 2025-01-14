5e8f4d5725c1c25bf66b5d1c020ee5122258b1e0
JohnDominicJasmin <johndominicjasmin@gmail.com>
2025-01-15 02:43:57 +0800

    Bugfix: UI is below the notification makes it to exceed the screen

--- app/src/main/java/com/example/codingchallenge/MainActivity.kt
+++ app/src/main/java/com/example/codingchallenge/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
@@ -24,24 +25,17 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             CodingChallengeTheme {
-                AppNavigator()
+                Scaffold(
+
+                    content = { paddingValues ->
+                        Box(modifier = Modifier.padding(paddingValues)) {
+                            AppNavigator()
+                        }
+                    }
+                )
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    CodingChallengeTheme {
-        Greeting("Android")
-    }
-}
